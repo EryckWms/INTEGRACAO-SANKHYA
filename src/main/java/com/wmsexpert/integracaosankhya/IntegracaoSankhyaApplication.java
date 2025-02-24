ac117361e8ee84e780353c88902e4c4763022605
eryckavel@wmsexpert.com.br <eryckavel@wmsexpert.com.br>
2025-02-24 15:54:36 -0300

    initial commit project

---
+++ src/main/java/com/wmsexpert/integracaosankhya/IntegracaoSankhyaApplication.java
@@ -0,0 +1,13 @@
+package com.wmsexpert.integracaosankhya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoSankhyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoSankhyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wmsexpert/integracaosankhya/IntegracaoSankhyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmsexpert.integracaosankhya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoSankhyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
